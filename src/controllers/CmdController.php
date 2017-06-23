0254b420552e1efd6e77f68183828ffdeaa40306
Vitaliy <theyamshikov@yandex.ru>
2017-06-23 16:23:38 +0600

    update namespace

--- src/controllers/CmdController.php
+++ src/controllers/CmdController.php
@@ -3,10 +3,10 @@
 namespace yii2module\test\controllers;
 
 use Yii;
-use woop\extension\console\yii\console\Controller;
+use yii2lab\console\yii\console\Controller;
 use yii2module\test\helpers\Test;
-use woop\extension\console\helpers\input\Question;
-use woop\extension\console\helpers\Output;
+use yii2lab\console\helpers\input\Question;
+use yii2lab\console\helpers\Output;
 
 class CmdController extends Controller
 {
